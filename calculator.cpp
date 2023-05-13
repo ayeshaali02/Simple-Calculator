2b42ad54ecb10a36a540949a692b497656df16da
Ayesha Ali <104257872+ayeshaali02@users.noreply.github.com>
2023-05-13 22:27:54 +0530

    calculator.cpp

--- calculator.cpp
+++ calculator.cpp
@@ -5,16 +5,16 @@ int main(){
     cout << "Enter the operator and the two numbers: ";
     cin >> op >> a >> b;
     switch(op){
-        case 1:
-            cout << a+b;
+        case 1: //addition
+            cout << a+b; 
             break;
-        case 2:
+        case 2: //subtraction
             cout << a-b;
             break;
-        case 3:
+        case 3: //division
             cout << a/b;
             break;
-        case 4:
+        case 4: //multiplication
             cout << a*b;
             break;
         default:
